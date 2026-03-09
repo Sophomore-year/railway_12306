d9ea8d49be641c6e8cd9d6c901f156a4f19c5d5c
Sophomore-year <18908639587@163.com>
2026-03-09 21:44:10 +0800

    错误码接口

---
+++ frameworks/convention-spring-boot-starter/src/main/java/org/zys/railway_12306/framework/starter/convention/errorcode/IErrorCode.java
@@ -0,0 +1,16 @@
+package org.zys.railway_12306.framework.starter.convention.errorcode;
+
+/*
+ * 错误码接口
+ */
+public interface IErrorCode {
+    /*
+     * 错误码
+     */
+    String code();
+
+    /*
+     * 错误信息
+     */
+    String message();
+}

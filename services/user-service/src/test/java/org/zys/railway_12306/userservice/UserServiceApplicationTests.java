ac2b80d7b7f713e1db47efd044255e66a24201e1
Sophomore-year <18908639587@163.com>
2026-03-09 15:29:56 +0800

    基本框架

---
+++ services/user-service/src/test/java/org/zys/railway_12306/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zys.railway_12306.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

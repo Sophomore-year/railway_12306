f74cb7f616d4a95530efc7f9c79cbc2973668f58
Sophomore-year <18908639587@163.com>
2026-03-09 21:38:32 +0800

    用户信息实体

---
+++ services/user-service/src/main/java/org/zys/railway_12306/userservice/pojo/entity/User.java
@@ -0,0 +1,90 @@
+package org.zys.railway_12306.userservice.pojo.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.zys.rail_12306.framework.starter.database.base.BaseDO;
+
+/*
+ * 用户信息实体
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("t_user")
+public class User extends BaseDO {
+
+    /*
+     * id
+     */
+    private Long id;
+
+    /*
+     * 用户名
+     */
+    private String username;
+
+    /*
+     * 密码
+     */
+    private String password;
+
+    /*
+     * 真实姓名
+     */
+    private String realName;
+
+    /*
+     * 国家/地区
+     */
+    private String region;
+
+    /*
+     * 证件类型
+     */
+    private Integer idType;
+
+    /*
+     * 证件号
+     */
+    private String idCard;
+
+    /*
+     * 手机号
+     */
+    private String phone;
+
+    /*
+     * 固定电话
+     */
+    private String telephone;
+
+    /*
+     * 邮箱
+     */
+    private String mail;
+
+    /*
+     * 旅客类型
+     */
+    private Integer userType;
+
+    /*
+     * 审核状态
+     */
+    private Integer verifyStatus;
+
+    /*
+     * 邮编
+     */
+    private String postCode;
+
+    /*
+     * 地址
+     */
+    private String address;
+
+    /*
+     * 注销时间戳
+     */
+    private Long deletionTime;
+}

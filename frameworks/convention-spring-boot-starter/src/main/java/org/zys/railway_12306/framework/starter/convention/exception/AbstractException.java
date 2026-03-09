888911d481dc3d27d5918165dc1e7bf764c56d43
Sophomore-year <18908639587@163.com>
2026-03-09 21:48:46 +0800

    抽象项目中三类异常体系，客户端异常、服务端异常以及远程服务调用异常

---
+++ frameworks/convention-spring-boot-starter/src/main/java/org/zys/railway_12306/framework/starter/convention/exception/AbstractException.java
@@ -0,0 +1,21 @@
+package org.zys.railway_12306.framework.starter.convention.exception;
+
+import com.google.common.base.Strings;
+import org.zys.railway_12306.framework.starter.convention.errorcode.IErrorCode;
+
+import java.util.Optional;
+
+/*
+* 抽象项目中三类异常体系，客户端异常、服务端异常以及远程服务调用异常
+* */
+public abstract class AbstractException extends RuntimeException {
+    public final String errorCode;
+
+    public final String errorMessage;
+
+    public AbstractException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable);
+        this.errorCode = errorCode.code();
+        this.errorMessage = Optional.ofNullable(Strings.emptyToNull(message)).orElse(errorCode.message());
+    }
+}

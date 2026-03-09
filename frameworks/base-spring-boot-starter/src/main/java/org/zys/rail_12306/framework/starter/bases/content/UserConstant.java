9e88ea0ec29d73cb255be83245a4122a78e23a8a
Sophomore-year <18908639587@163.com>
2026-03-09 21:38:08 +0800

    用户常量

---
+++ frameworks/base-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/bases/content/UserConstant.java
@@ -0,0 +1,23 @@
+package org.zys.rail_12306.framework.starter.bases.content;
+
+
+/*
+* 用户常量
+* */
+public final class UserConstant {
+
+    /*
+     * 用户 ID Key
+     */
+    public static final String USER_ID_KEY = "userId";
+
+    /*
+     * 用户名 Key
+     */
+    public static final String USER_NAME_KEY = "username";
+
+    /*
+     * 用户真实名称 Key
+     */
+    public static final String REAL_NAME_KEY = "realName";
+}

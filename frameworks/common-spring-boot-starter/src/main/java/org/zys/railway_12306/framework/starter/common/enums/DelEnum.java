0b6caeaa87f11e60181ff70bbb11e67de4ff2e8c
Sophomore-year <18908639587@163.com>
2026-03-10 13:40:59 +0800

    删除标记枚举

---
+++ frameworks/common-spring-boot-starter/src/main/java/org/zys/railway_12306/framework/starter/common/enums/DelEnum.java
@@ -0,0 +1,38 @@
+package org.zys.railway_12306.framework.starter.common.enums;
+
+/**
+ *删除标记枚举
+ *
+ * @author SUM
+ * @date 2026/03/10
+ */
+public enum DelEnum {
+    /**
+     * 正常状态
+     */
+    NORMAL(0),
+
+    /**
+     * 删除状态
+     */
+    DELETE(1);
+
+    private final Integer statusCode;
+
+    DelEnum(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Integer code() {
+        return this.statusCode;
+    }
+
+    public String strCode() {
+        return String.valueOf(this.statusCode);
+    }
+
+    @Override
+    public String toString() {
+        return strCode();
+    }
+}

e054a5eb0e06c3822ffad50e1611f032ac3f096e
Sophomore-year <18908639587@163.com>
2026-03-10 14:08:03 +0800

    MybatisPlus 配置文件

---
+++ frameworks/database-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/database/config/MybatisPlusAutoConfiguration.java
@@ -0,0 +1,24 @@
+package org.zys.rail_12306.framework.starter.database.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ *MybatisPlus 配置文件
+ *
+ * @author SUM
+ * @date 2026/03/10
+ */
+public class MybatisPlusAutoConfiguration {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}

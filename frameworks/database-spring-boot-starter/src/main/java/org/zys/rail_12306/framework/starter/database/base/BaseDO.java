6931ca6a410dde4f9283797cda4d549d51f5519f
Sophomore-year <18908639587@163.com>
2026-03-10 14:08:23 +0800

    数据持久层基础属性

--- frameworks/database-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/database/base/BaseDO.java
+++ frameworks/database-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/database/base/BaseDO.java
@@ -6,25 +6,25 @@ import lombok.Data;
 
 import java.util.Date;
 
-/*
+/**
  * 数据持久层基础属性
  */
 @Data
 public class BaseDO {
 
-    /*
+    /**
      * 创建时间
      */
     @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
-    /*
+    /**
      * 修改时间
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
-    /*
+    /**
      * 删除标志
      */
     @TableField(fill = FieldFill.INSERT)

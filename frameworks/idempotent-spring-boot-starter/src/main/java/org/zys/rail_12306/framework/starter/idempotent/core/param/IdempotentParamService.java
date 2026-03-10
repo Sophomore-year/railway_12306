d507ac390085f32074c384ba1baa48808b894708
Sophomore-year <18908639587@163.com>
2026-03-10 16:36:39 +0800

    参数方式幂等实现接口

---
+++ frameworks/idempotent-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/idempotent/core/param/IdempotentParamService.java
@@ -0,0 +1,12 @@
+package org.zys.rail_12306.framework.starter.idempotent.core.param;
+
+import org.zys.rail_12306.framework.starter.idempotent.core.IdempotentExecuteHandler;
+
+/**
+ *参数方式幂等实现接口
+ *
+ * @author SUM
+ * @date 2026/03/10
+ */
+public interface IdempotentParamService extends IdempotentExecuteHandler {
+}

176c6a32224dc3335b632aa39415543d14cb3710
Sophomore-year <18908639587@163.com>
2026-03-10 16:34:19 +0800

    SpEL 方式幂等实现接口

---
+++ frameworks/idempotent-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/idempotent/core/spel/IdempotentSpELService.java
@@ -0,0 +1,12 @@
+package org.zys.rail_12306.framework.starter.idempotent.core.spel;
+
+import org.zys.rail_12306.framework.starter.idempotent.core.IdempotentExecuteHandler;
+
+/**
+ *
+ *SpEL 方式幂等实现接口
+ * @author SUM
+ * @date 2026/03/10
+ */
+public interface IdempotentSpELService extends IdempotentExecuteHandler {
+}

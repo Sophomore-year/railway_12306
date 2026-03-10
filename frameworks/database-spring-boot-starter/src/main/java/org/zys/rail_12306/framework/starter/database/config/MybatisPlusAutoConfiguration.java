027747b3692e02762470e94b6575bee53ef49210
Sophomore-year <18908639587@163.com>
2026-03-10 14:13:14 +0800

    自定义雪花算法 ID 生成器

--- frameworks/database-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/database/config/MybatisPlusAutoConfiguration.java
+++ frameworks/database-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/database/config/MybatisPlusAutoConfiguration.java
@@ -1,9 +1,12 @@
 package org.zys.rail_12306.framework.starter.database.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.zys.rail_12306.framework.starter.database.handler.CustomIdGenerator;
 
 /**
  *MybatisPlus 配置文件
@@ -20,5 +23,14 @@ public class MybatisPlusAutoConfiguration {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    /**
+     * 自定义雪花算法 ID 生成器
+     */
+    @Bean
+    @Primary
+    public IdentifierGenerator idGenerator() {
+        return new CustomIdGenerator();
     }
 }

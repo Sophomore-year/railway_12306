9f0db0dcb34fcb70bf9ef297dada26f12c072e99
Sophomore-year <18908639587@163.com>
2026-03-14 15:37:03 +0800

    用户相关责任链 Mark 枚举

---
+++ services/user-service/src/main/java/org/zys/railway_12306/service/user/enums/UserChainMarkEnum.java
@@ -0,0 +1,14 @@
+package org.zys.railway_12306.service.user.enums;
+
+/**
+ *用户相关责任链 Mark 枚举
+ *
+ * @author SUM
+ * @date 2026/03/14
+ */
+public enum UserChainMarkEnum {
+    /**
+     * 用户注册过滤器
+     */
+    USER_REGISTER_FILTER
+}

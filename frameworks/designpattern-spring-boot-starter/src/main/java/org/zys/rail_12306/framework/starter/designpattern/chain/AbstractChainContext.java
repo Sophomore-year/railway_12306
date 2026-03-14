368221ea7953d944822ad97e36301e4e34ebaff0
Sophomore-year <18908639587@163.com>
2026-03-14 15:14:03 +0800

    抽象责任链上下文

---
+++ frameworks/designpattern-spring-boot-starter/src/main/java/org/zys/rail_12306/framework/starter/designpattern/chain/AbstractChainContext.java
@@ -0,0 +1,53 @@
+package org.zys.rail_12306.framework.starter.designpattern.chain;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.util.CollectionUtils;
+import org.zys.rail_12306.framework.starter.bases.ApplicationContextHolder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *抽象责任链上下文
+ *
+ * @author SUM
+ * @date 2026/03/14
+ */
+public class AbstractChainContext<T> implements CommandLineRunner {
+    private final Map<String, List<AbstractChainHandler>> abstractChainHandlerContainer = new HashMap<>();
+
+    /**
+     * 责任链组件执行
+     *
+     * @param mark         责任链组件标识
+     * @param requestParam 请求参数
+     */
+    public void handler(String mark, T requestParam) {
+        List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(mark);
+        if (CollectionUtils.isEmpty(abstractChainHandlers)) {
+            throw new RuntimeException(String.format("[%s] Chain of Responsibility ID is undefined.", mark));
+        }
+        abstractChainHandlers.forEach(each -> each.handler(requestParam));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, AbstractChainHandler> chainFilterMap = ApplicationContextHolder
+                .getBeansOfType(AbstractChainHandler.class);
+        chainFilterMap.forEach((beanName, bean) -> {
+            List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(bean.mark());
+            if (CollectionUtils.isEmpty(abstractChainHandlers)) {
+                abstractChainHandlers = new ArrayList();
+            }
+            abstractChainHandlers.add(bean);
+            List<AbstractChainHandler> actualAbstractChainHandlers = abstractChainHandlers.stream()
+                    .sorted(Comparator.comparing(Ordered::getOrder))
+                    .collect(Collectors.toList());
+            abstractChainHandlerContainer.put(bean.mark(), actualAbstractChainHandlers);
+        });
+    }
+}

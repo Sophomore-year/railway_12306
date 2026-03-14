f9986f9b53b827e10c516b20ed7dfbca1fdfbb3a
Sophomore-year <18908639587@163.com>
2026-03-14 15:56:17 +0800

    系统级公共常量

---
+++ services/user-service/src/main/java/org/zys/railway_12306/service/user/constant/Railway12306Constant.java
@@ -0,0 +1,14 @@
+package org.zys.railway_12306.service.user.constant;
+
+/**
+ *系统级公共常量
+ *
+ * @author SUM
+ * @date 2026/03/14
+ */
+public class Railway12306Constant {
+    /**
+     * 用户注册可复用用户名分片数
+     */
+    public static final int USER_REGISTER_REUSE_SHARDING_COUNT = 1024;
+}

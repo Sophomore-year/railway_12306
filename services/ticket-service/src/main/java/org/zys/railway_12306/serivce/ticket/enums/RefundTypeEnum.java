fbf70758dd10d4a4cc155866a8fb51547158995e
Sophomore-year <18908639587@163.com>
2026-03-18 19:26:00 +0800

    退款类型枚举

---
+++ services/ticket-service/src/main/java/org/zys/railway_12306/serivce/ticket/enums/RefundTypeEnum.java
@@ -0,0 +1,33 @@
+package org.zys.railway_12306.serivce.ticket.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ *退款类型枚举
+ *
+ * @author SUM
+ * @date 2026/03/18
+ */
+@Getter
+@RequiredArgsConstructor
+public enum RefundTypeEnum {
+
+    /**
+     * 部分退款
+     */
+    PARTIAL_REFUND(11, 0, "PARTIAL_REFUND", "部分退款"),
+
+    /**
+     * 全部退款
+     */
+    FULL_REFUND(12, 1, "FULL_REFUND", "全部退款");
+
+    private final Integer code;
+
+    private final Integer type;
+
+    private final String name;
+
+    private final String value;
+}

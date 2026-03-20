7c2d4cd8a431a54d882c694364e605e06d11ea7b
Sophomore-year <18908639587@163.com>
2026-03-20 15:23:13 +0800

    分页请求对象

--- frameworks/convention-spring-boot-starter/src/main/java/org/zys/railway_12306/framework/starter/convention/page/PageRequest.java
+++ frameworks/convention-spring-boot-starter/src/main/java/org/zys/railway_12306/framework/starter/convention/page/PageRequest.java
@@ -3,17 +3,20 @@ package org.zys.railway_12306.framework.starter.convention.page;
 import lombok.Data;
 
 
-/*
-* 分页请求对象
-* */
+/**
+ * 分页请求对象
+ *
+ * @author SUM
+ * @date 2026/03/20
+ */
 @Data
 public class PageRequest {
-    /*
+    /**
      * 当前页
      */
     private Long current = 1L;
 
-    /*
+    /**
      * 每页显示条数
      */
     private Long size = 10L;

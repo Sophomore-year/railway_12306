22b702039e7a55c2e06d5770a94e2d7e16d7ff8a
Sophomore-year <18908639587@163.com>
2026-03-21 13:59:13 +0800

    根据列车 ID 查询站点信息

---
+++ services/ticket-service/src/main/java/org/zys/railway_12306/serivce/ticket/controller/TrainStationController.java
@@ -0,0 +1,31 @@
+package org.zys.railway_12306.serivce.ticket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zys.railway_12306.framework.starter.convention.result.Result;
+import org.zys.railway_12306.framework.starter.web.Results;
+import org.zys.railway_12306.serivce.ticket.pojo.dto.resp.TrainStationQueryRespDTO;
+import org.zys.railway_12306.serivce.ticket.service.TrainStationService;
+
+import java.util.List;
+
+/**
+ *列车站点控制层
+ *
+ * @author SUM
+ * @date 2026/03/20
+ */
+@RestController
+@RequiredArgsConstructor
+public class TrainStationController {
+    private final TrainStationService trainStationService;
+
+    /**
+     * 根据列车 ID 查询站点信息
+     */
+    @GetMapping("/api/ticket-service/train-station/query")
+    public Result<List<TrainStationQueryRespDTO>> listTrainStationQuery(String trainId) {
+        return Results.success(trainStationService.listTrainStationQuery(trainId));
+    }
+}

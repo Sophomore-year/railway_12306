e0ce69f3f916693ac0c400f7af4b5cb525fdf0e6
Sophomore-year <18908639587@163.com>
2026-03-22 13:39:24 +0800

    列车站点控制层

--- services/ticket-service/src/main/java/org/zys/railway_12306/service/ticket/controller/TrainStationController.java
+++ services/ticket-service/src/main/java/org/zys/railway_12306/service/ticket/controller/TrainStationController.java
@@ -1,12 +1,12 @@
-package org.zys.railway_12306.serivce.ticket.controller;
+package org.zys.railway_12306.service.ticket.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zys.railway_12306.framework.starter.convention.result.Result;
 import org.zys.railway_12306.framework.starter.web.Results;
-import org.zys.railway_12306.serivce.ticket.pojo.dto.resp.TrainStationQueryRespDTO;
-import org.zys.railway_12306.serivce.ticket.service.TrainStationService;
+import org.zys.railway_12306.service.ticket.pojo.dto.resp.TrainStationQueryRespDTO;
+import org.zys.railway_12306.service.ticket.service.TrainStationService;
 
 import java.util.List;
 

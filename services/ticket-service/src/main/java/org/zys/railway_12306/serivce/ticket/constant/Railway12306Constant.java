c310d93f11c41a4a6ef1bc34ad16fb6b7815a458
Sophomore-year <18908639587@163.com>
2026-03-22 12:46:58 +0800

    系统级公共常量:提前买票天数

---
+++ services/ticket-service/src/main/java/org/zys/railway_12306/serivce/ticket/constant/Railway12306Constant.java
@@ -0,0 +1,14 @@
+package org.zys.railway_12306.serivce.ticket.constant;
+
+/**
+ *系统级公共常量
+ *
+ * @author SUM
+ * @date 2026/03/19
+ */
+public final class Railway12306Constant {
+    /**
+     * 提前买票天数
+     */
+    public static final int ADVANCE_TICKET_DAY = 15;
+}

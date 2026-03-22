1047e365cb3a815db73d76b675118276e26d79ca
Sophomore-year <18908639587@163.com>
2026-03-22 12:57:16 +0800

    地区以及车站接口层

---
+++ services/ticket-service/src/main/java/org/zys/railway_12306/serivce/ticket/service/RegionStationService.java
@@ -0,0 +1,30 @@
+package org.zys.railway_12306.serivce.ticket.service;
+
+import org.zys.railway_12306.serivce.ticket.pojo.dto.req.RegionStationQueryReqDTO;
+import org.zys.railway_12306.serivce.ticket.pojo.dto.resp.RegionStationQueryRespDTO;
+import org.zys.railway_12306.serivce.ticket.pojo.dto.resp.StationQueryRespDTO;
+
+import java.util.List;
+
+/**
+ *地区以及车站接口层
+ *
+ * @author SUM
+ * @date 2026/03/21
+ */
+public interface RegionStationService {
+    /**
+     * 查询车站&城市站点集合信息
+     *
+     * @param requestParam 车站&站点查询参数
+     * @return 车站&站点返回数据集合
+     */
+    List<RegionStationQueryRespDTO> listRegionStation(RegionStationQueryReqDTO requestParam);
+
+    /**
+     * 查询所有车站&城市站点集合信息
+     *
+     * @return 车站返回数据集合
+     */
+    List<StationQueryRespDTO> listAllStation();
+}

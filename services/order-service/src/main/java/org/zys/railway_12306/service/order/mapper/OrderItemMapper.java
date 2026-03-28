1e67804ab1d89f6c28ea4c10795c36c40dc3ba1b
Sophomore-year <18908639587@163.com>
2026-03-28 12:05:18 +0800

    docs(mapper): 添加订单相关持久层接口注释
    - 为 OrderItemMapper 接口添加类注释说明其功能
    - 为 OrderMapper 接口添加类注释说明其功能
    - 统一添加作者和日期信息到相关映射器接口

--- services/order-service/src/main/java/org/zys/railway_12306/service/order/mapper/OrderItemMapper.java
+++ services/order-service/src/main/java/org/zys/railway_12306/service/order/mapper/OrderItemMapper.java
@@ -3,5 +3,11 @@ package org.zys.railway_12306.service.order.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.zys.railway_12306.service.order.pojo.entity.OrderItem;
 
+/**
+ *订单明细持久层
+ *
+ * @author SUM
+ * @date 2026/03/28
+ */
 public interface OrderItemMapper extends BaseMapper<OrderItem> {
 }
--- services/order-service/src/main/java/org/zys/railway_12306/service/order/mapper/OrderMapper.java
+++ services/order-service/src/main/java/org/zys/railway_12306/service/order/mapper/OrderMapper.java
@@ -3,5 +3,11 @@ package org.zys.railway_12306.service.order.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.zys.railway_12306.service.order.pojo.entity.Order;
 
+/**
+ *订单持久层
+ *
+ * @author SUM
+ * @date 2026/03/28
+ */
 public interface OrderMapper extends BaseMapper<Order> {
 }

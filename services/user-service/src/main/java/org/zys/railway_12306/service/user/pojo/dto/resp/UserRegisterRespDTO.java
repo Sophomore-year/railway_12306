270888a5b53638f462e3642615f5d59048033ee8
Sophomore-year <18908639587@163.com>
2026-04-02 22:34:34 +0800

    refactor(user): 优化用户注册响应DTO结构
    - 添加Lombok Data注解简化代码
    - 移除手动getter/setter方法提升可读性
    - 保持原有字段结构不变确保兼容性
    - 减少样板代码提高维护效率

--- services/user-service/src/main/java/org/zys/railway_12306/service/user/pojo/dto/resp/UserRegisterRespDTO.java
+++ services/user-service/src/main/java/org/zys/railway_12306/service/user/pojo/dto/resp/UserRegisterRespDTO.java
@@ -1,11 +1,14 @@
 package org.zys.railway_12306.service.user.pojo.dto.resp;
 
+import lombok.Data;
+
 /**
  *用户注册返回参数
  *
  * @author SUM
  * @date 2026/03/11
  */
+@Data
 public class UserRegisterRespDTO {
     /**
      * 用户名
